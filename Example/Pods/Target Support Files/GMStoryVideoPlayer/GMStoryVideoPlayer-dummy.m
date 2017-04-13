03c215d036a32a042ef38a31da69549f406c8614
Gaston Morixe <gaston@black.uy>
2017-04-13 19:05:01 -0300

    Init

---
+++ Example/Pods/Target Support Files/GMStoryVideoPlayer/GMStoryVideoPlayer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GMStoryVideoPlayer : NSObject
+@end
+@implementation PodsDummy_GMStoryVideoPlayer
+@end
---
+++ Example/Pods/Target Support Files/Pods-GMStoryVideoPlayer_Tests/Pods-GMStoryVideoPlayer_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GMStoryVideoPlayer_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_GMStoryVideoPlayer_Tests
+@end
